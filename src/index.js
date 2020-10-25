180c95d44158dee50e72b27ab644fb370d5894c6
KaterinaPrannik <60596523+KaterinaPrannik@users.noreply.github.com>
2020-10-25 21:59:05 +0300

    fix: add decide

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
-// You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let sortArray=[];
+  if(!Array.isArray(matrix)){
+    return [];
+  } else {
+    for( let i = 0; i<matrix.length; i++){
+      let item = matrix[i];
+      if(i %2 !== 0){
+       item = item.reverse();
+
+      }
+      sortArray.push(...item);
+    }
+  }
+  return sortArray;
 }
